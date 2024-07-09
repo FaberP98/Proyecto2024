099ed299ea714961e81f770e5405889c4a60c70a
FaberP98 <159584213+FaberP98@users.noreply.github.com>
2024-07-09 15:06:04 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <?php include('vistas/custom_header.html'); ?>
+</head>
+<body>
+<?php include('vistas/nav.php'); ?>
+    
+</body>
+</html>
